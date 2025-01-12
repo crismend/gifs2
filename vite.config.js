720b4bf2852cd0086a2711b1c9f0a2c639d2b901
Cristian Mendoza <cfmendoza.audit@gmail.com>
2025-01-12 15:00:54 -0500

    act4

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './', // Base relativa para que funcione en GitHub Pages
+  plugins: [react()]
 })
